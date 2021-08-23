e2a4dd437515d5d5460c936ab6afac1ae81027b9
ayeong <ayoganatanatda@naver.com>
2021-08-23 09:06:26 +0900

    menu trying ...

--- kakaologin-android-sample-master/app/src/main/java/com/kakaologin_sample/MainActivity.java
+++ kakaologin-android-sample-master/app/src/main/java/com/kakaologin_sample/MainActivity.java
@@ -1,28 +1,48 @@
 package com.kakaologin_sample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-    Button scanQR, mypage;
+import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        scanQR = findViewById(R.id.scanQR);
-        scanQR.setOnClickListener(new View.OnClickListener() {
+
+        final DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        final View drawerView = (View) findViewById(R.id.drawer);
+        Button btnOpenDrawer = (Button)findViewById(R.id.btn_OpenDrawer);
+        Button btnCloseDrawer = (Button)findViewById(R.id.btn_CloseDrawer);
+        Button qrCode = (Button)findViewById(R.id.qr_code);
+        Button myPage = (Button)findViewById(R.id.mypage);
+
+        btnOpenDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+        btnCloseDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawer(drawerView);
+            }
+        });
+        qrCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, ScanQR.class);
                 startActivity(i);
             }
         });
-        mypage = findViewById(R.id.mypage);
-        mypage.setOnClickListener(new View.OnClickListener() {
+        myPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, MyPage.class);
